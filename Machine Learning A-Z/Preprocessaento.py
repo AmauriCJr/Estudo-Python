be2e5e01b3cc5202fdb527cb0e7487d3e8c3f2e5
AmauriCJr <amauri_cj@hotmail.com>
2022-05-02 15:11:53 -0300

    Update Preprocessaento.py

--- Machine Learning A-Z/Preprocessaento.py
+++ Machine Learning A-Z/Preprocessaento.py
@@ -1,6 +1,10 @@
+#Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+#Importing the dataset
 
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
@@ -10,12 +14,18 @@ print(x)
 
 print(y)
 
+#Taking care of missing data
+
 from sklearn.impute import SimpleImputer #biblioteca que possui ferramentas de preprocessamento de dados
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean') #escolher substituir valores faltantantes pela média
 imputer.fit(x[:, 1:3]) #seleciona as colunas que vaõ ser analisadas
 x[:, 1:3] = imputer.transform(x[:, 1:3]) #transforma as colunas de acordo com o que especificado
 
 print(x)
+
+#Encoding categorical data
+
+#Encoding the Independent Variable
 
 #irá tratar a primeira coluna que possui o nome dos países para que sejam organizadas de forma numérica sem ordem específica
 from sklearn.compose import ColumnTransformer
@@ -25,12 +35,16 @@ x = np.array(ct.fit_transform(x)) #faz o fit e o transform ao mesmo tempo, difer
 
 print(x)
 
+#Encoding the Dependent Variable
+
 #organiza as variáveis dependentes da mesma forma que a primeria coluna das independentes
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder() #cria a regra
 y = le.fit_transform(y)
 
 print(y)
+
+#Splitting the dataset into the Training set and Test set
 
 #divide os dados em duas partes, uma parte que servirá pra treinar a máquina e outra que servirá de teste
 from sklearn.model_selection import train_test_split
@@ -43,6 +57,8 @@ print(x_test)
 print(y_train)
 
 print(y_test)
+
+#Feature Scaling
 
 #normaliza as variáveis para o processamento
 from sklearn.preprocessing import StandardScaler
