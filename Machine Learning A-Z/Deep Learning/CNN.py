92add54df47da2f851bb3b3e3b4708dab1ec3fba
Amauri da Costa Júnior <amauri_cj@hotmail.com>
2022-07-05 12:34:53 -0300

    Update CNN.py

--- Machine Learning A-Z/Deep Learning/CNN.py
+++ Machine Learning A-Z/Deep Learning/CNN.py
@@ -1,15 +1,9 @@
-### Importing the libraries
-"""
-
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 
 tf.__version__
 
-"""## Part 1 - Data Preprocessing
-
-### Preprocessing the Training set
-"""
+#Preprocessing the Training set
 
 #Image Augmentation
 train_datagen = ImageDataGenerator(
@@ -23,7 +17,7 @@ training_set = train_datagen.flow_from_directory(
     batch_size = 32,
     class_mode = 'binary')
 
-"""### Preprocessing the Test set"""
+# Preprocessing the Test set
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory(
@@ -32,14 +26,12 @@ test_set = test_datagen.flow_from_directory(
     batch_size = 32,
     class_mode = 'binary')
 
-"""## Part 2 - Building the CNN
 
-### Initialising the CNN
-"""
+# Initialising the CNN
 
 cnn = tf.keras.models.Sequential()
 
-"""### Step 1 - Convolution"""
+#Convolution
 
 cnn.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', input_shape = [64, 64, 3]))
 #filters - número de filtros
@@ -47,48 +39,44 @@ cnn.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu
 #activation - tipo de filtro aplicado ao pesos das sinapses, no caso retificador
 #input_shape - formato do dado de entrada, só é colocodao na input layer
 
-"""### Step 2 - Pooling"""
+#Pooling
 
 cnn.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
 #pool_size - tamanho da matriz que forma o pooled feature map
 #strides - valor que a matriz que forma o pooled feature map anda para o lado
 
-"""### Adding a second convolutional layer"""
+#Adding a second convolutional layer
 
 cnn.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu'))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
 
-"""### Step 3 - Flattening"""
+#Flattening
 
 #Transforma as matrizes em vetores
 cnn.add(tf.keras.layers.Flatten())
 
-"""### Step 4 - Full Connection"""
+#Full Connection
 
 cnn.add(tf.keras.layers.Dense(units = 128, activation = 'relu'))
 #units - número de neurônios
 
-"""### Step 5 - Output Layer"""
+#Output Layer
 
 cnn.add(tf.keras.layers.Dense(units = 1, activation = 'sigmoid'))
 
-"""## Part 3 - Training the CNN
-
-### Compiling the CNN
-"""
+# Compiling the CNN
 
 cnn.compile(optimizer = 'adam',loss = 'binary_crossentropy',metrics = ['accuracy'])
 
-"""### Training the CNN on the Training set and evaluating it on the Test set"""
+# Training the CNN on the Training set and evaluating it on the Test set
 
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
 
-"""## Part 4 - Making a single prediction"""
+#Making a single prediction
 
 import numpy as np
-from keras.preprocessing import image
-test_image = image.load_img('D:/Backup/Documents/Machine Learning A-Z (Codes and Datasets)/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
+test_image = tf.keras.preprocessing.image.load_img('D:/Backup/Documents/Machine Learning A-Z (Codes and Datasets)/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
+test_image = tf.keras.preprocessing.image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = cnn.predict(test_image)
 print("Índices = ",training_set.class_indices)
